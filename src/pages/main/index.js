e44cb8f97751a67b39428a3c94ec06895c825cd3
Gabriel Eloy <gabrieleloy@Gabriels-MacBook-Pro.local>
2021-12-19 20:39:08 -0300

    🎨 code enhancements

---
+++ src/pages/main/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {View, SafeAreaView} from 'react-native';
+import TotalArea from './totalArea';
+import DescriptionArea from './DescriptionArea';
+import {getTotal} from '../../services/balance';
+import {useTransactions} from '../..//hooks/useTransactions';
+import styles from './style';
+
+const Main = () => {
+  const {transactions, addTransaction, deleteTransaction} = useTransactions();
+
+  const total = getTotal(transactions);
+
+  return (
+    <View style={styles.contentArea}>
+      <SafeAreaView style={styles.appContainer}>
+        <View style={styles.totalContainer}>
+          <TotalArea
+            addDebit={() => addTransaction(100, true)}
+            addCredit={() => addTransaction(100, false)}
+            total={total}
+          />
+        </View>
+        <View style={styles.bodyContainer}>
+          <DescriptionArea
+            deleteTransaction={deleteTransaction}
+            transactions={transactions}
+          />
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default Main;
---
+++ src/pages/main/style.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+  contentArea: {flex: 1, backgroundColor: '#2D2D38'},
+  totalContainer: {
+    flex: 3,
+  },
+  bodyContainer: {
+    flex: 7,
+    paddingHorizontal: 20,
+  },
+});
