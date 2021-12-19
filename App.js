4d2f581435c71be743c68107ba4ee32d765e7ff9
Gabriel Eloy <gabrieleloy@Gabriels-MacBook-Pro.local>
2021-12-19 20:10:24 -0300

    ♻️ moved transactions logic to its own hook

--- App.js
+++ App.js
@@ -2,34 +2,13 @@ import React from 'react';
 import {View, StyleSheet, SafeAreaView} from 'react-native';
 import TotalArea from './src/pages/main/totalArea';
 import DescriptionArea from './src/pages/main/DescriptionArea';
-import useSWR, {useSWRConfig} from 'swr';
-import axios from 'axios';
 import {getTotal} from './src/services/balance';
-import {fetcher} from './src/services/network';
+import {useTransactions} from './src/hooks/useTransactions';
 
 const App = () => {
-  const urlPath = `${process.env.API_BASE_URL}/transactions`;
-  const {data: transactions} = useSWR(urlPath, fetcher);
-  const {mutate} = useSWRConfig();
-  
+  const {transactions, addTransaction, deleteTransaction} = useTransactions();
+
   const total = getTotal(transactions);
-  
-
-  const addTransaction = async (value, isDebit) => {
-    const type = isDebit ? 'debit' : 'credit';
-    const transaction = {
-      value,
-      type,
-    };
-    await axios.post(urlPath, transaction);
-    mutate(urlPath);
-    return;
-  };
-
-  const deleteTransaction = async id => {
-    await axios.delete(`${urlPath}/${id}`);
-    mutate(urlPath);
-  };
 
   return (
     <View style={styles.contentArea}>
