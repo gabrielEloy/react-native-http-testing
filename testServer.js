a4516773a1140ce9dcb2c9055f9d70e63c687400
Gabriel Eloy <gabrieleloy@Gabriels-MacBook-Pro.local>
2021-12-22 22:26:03 -0300

    changed testServer mock function in msw

--- testServer.js
+++ testServer.js
@@ -1,19 +1,33 @@
 import {rest} from 'msw';
 import {setupServer} from 'msw/node';
 
-const server = setupServer(
+const initialData = [
+  {
+    value: 365,
+    type: 'credit',
+    id: 1,
+  },
+]
+
+let data = initialData
+
+const endpointMocks = [
   rest.get('http://localhost:3000/transactions', (_req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json([
-        {
-          value: 365,
-          type: 'credit',
-          id: 1,
-        },
-      ]),
+      ctx.json(data),
     );
   }),
+  rest.post('http://localhost:3000/transactions', (_req, res, ctx) => {
+    data.push(_req.body);
+    return res(
+      ctx.status(200),
+      ctx.json(data),
+    );
+  })]
+
+const server = setupServer(
+  ...endpointMocks,
   rest.get('*', (req, res, ctx) => {
     console.error(`Please add request handler for ${req.url.toString()}`);
     return res(
@@ -25,6 +39,17 @@ const server = setupServer(
 
 export {server, rest};
 
+let afterEachCounter = 0;
+
 beforeAll(() => server.listen());
 afterAll(() => server.close());
-afterEach(() => server.resetHandlers());
+beforeEach(() => {
+    
+  data = initialData;
+  afterEachCounter += 1
+  console.log(`after each: ${afterEachCounter}`)
+  console.log(data)
+})
+afterEach(() => {
+  server.resetHandlers();
+});
